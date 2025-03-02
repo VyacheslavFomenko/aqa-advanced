d3a06037758efbdb1e49ceebd67b647b6e2156dd
Vyacheslav Fomenko <playfog3@gmail.com>
2025-03-02 10:19:21 +0200

    add: task 1 solution

---
+++ jsBasics/task1.js
@@ -0,0 +1,12 @@
+let str = "First task";
+let num = 42;
+let isTrue = true;
+let nullableVar = null;
+let undefinedVar;
+
+
+console.log("String:", str);
+console.log("Number:", num);
+console.log("Bool:", isTrue);
+console.log("Null:", nullableVar);
+console.log("Undefined:", undefinedVar);
