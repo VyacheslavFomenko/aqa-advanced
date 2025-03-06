a8a0f3fa60b5bff8af9912081182f47b7771340f
Vyacheslav Fomenko <playfog3@gmail.com>
2025-03-06 15:45:58 +0200

    add: first task solution

---
+++ fifthTask/first.js
@@ -0,0 +1,15 @@
+let averageGrade = parseFloat(prompt("Введіть вашу середню оцінку (0-100):"));
+
+if (averageGrade < 60) {
+    console.log("Незадовільно");
+} else if (averageGrade <= 70) {
+    console.log("Задовільно");
+} else if (averageGrade <= 80) {
+    console.log("Добре");
+} else if (averageGrade <= 90) {
+    console.log("Дуже добре");
+} else if (averageGrade <= 100) {
+    console.log("Відмінно");
+} else {
+    console.log("Некоректне значення оцінки");
+}
